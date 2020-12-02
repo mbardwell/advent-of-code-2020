92b186b9d5065a57a27eb4cb548a7fd9e43d85ff
Michael Bardwell <mdbardwe@ghgsat.com>
2020-12-01 21:04:27 -0700

    Revert "Add day1A"
    This reverts commit c097511713df7503e00cde6e81b44ceeac2f78b3.

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/mdbardwe/Documents/AoC2020/day1/target/debug/day1: /home/mdbardwe/Documents/AoC2020/day1/src/main.rs
--- day1/target/debug/deps/day1-5b8a2572d1cf922f.d
+++
@@ -1,5 +0,0 @@
-/home/mdbardwe/Documents/AoC2020/day1/target/debug/deps/day1-5b8a2572d1cf922f: src/main.rs
-
-/home/mdbardwe/Documents/AoC2020/day1/target/debug/deps/day1-5b8a2572d1cf922f.d: src/main.rs
-
-src/main.rs:
